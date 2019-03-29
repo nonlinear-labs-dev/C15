89339bea91032cf5442be22e023a78043898dd1d
Henry Hoegelow <henry@hoegelow.com>
2019-03-29 11:00:52 +0100

    fixup

--- audio-engine/src/synth/c15-audio-engine/ParameterStorage.h
+++ audio-engine/src/synth/c15-audio-engine/ParameterStorage.h
@@ -26,7 +26,6 @@ class ParameterStorage
 
     inline float operator=(float f)
     {
-      p->m_paramsignaldataV[voice][param] = f;
       p->m_paramsignaldataP[param][voice] = f;
       return f;
     }
@@ -49,29 +48,26 @@ class ParameterStorage
 
   inline float operator[](uint32_t param) const
   {
-    return m_boundVoice[param];
+    return get(m_voice, param);
   }
 
   inline Accessor operator[](uint32_t param)
   {
-    return Accessor(this, m_boundVoice[param], m_voice, param);
+    return Accessor(this, m_paramsignaldataP[param][m_voice], m_voice, param);
   }
 
   inline ParameterStorage &bindToVoice(uint32_t v)
   {
     m_voice = v;
-    m_boundVoice = m_paramsignaldataV[v];
     return *this;
   }
 
-  inline float get(uint32_t voice, uint32_t param)
+  inline float get(uint32_t voice, uint32_t param) const
   {
-    return m_paramsignaldataV[voice][param];
+    return m_paramsignaldataP[param][voice];
   }
 
  private:
-  float m_paramsignaldataV[dsp_number_of_voices][sig_number_of_signal_items] = {};
   FloatVector m_paramsignaldataP[sig_number_of_signal_items] = {};
-  float *m_boundVoice = m_paramsignaldataV[0];
   uint32_t m_voice = 0;
 };
