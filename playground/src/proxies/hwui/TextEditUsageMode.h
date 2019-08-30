5ac900790369216f86b7484b52959696dd582b48
schlenkibus <justus.scheil@parteimail.de>
2019-08-30 11:20:59 +0200

    wip

--- playground/src/proxies/hwui/TextEditUsageMode.h
+++ playground/src/proxies/hwui/TextEditUsageMode.h
@@ -20,6 +20,7 @@ class TextEditUsageMode : public UsageMode
 
   virtual bool onButtonPressed(Buttons buttonID, ButtonModifiers modifiers, bool state);
 
+  ustring getKeyLabel(Buttons buttonID) const;
   connection onTextChanged(const slot<void, const ustring &>& cb);
   ustring getKeyLabel(int buttonID) const;
 
