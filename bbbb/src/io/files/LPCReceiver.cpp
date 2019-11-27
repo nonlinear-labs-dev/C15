9d3b67bc12ead3a42a774fb77b2358ba7c13cc5d
schlenkibus <justus.scheil@parteimail.de>
2019-11-27 09:46:39 +0100

    wip

--- bbbb/src/io/files/LPCReceiver.cpp
+++ bbbb/src/io/files/LPCReceiver.cpp
@@ -14,10 +14,32 @@ LPCReceiver::LPCReceiver()
 
 LPCReceiver::~LPCReceiver() = default;
 
+void logHeartbeat(const Glib::RefPtr<Glib::Bytes> &bytes)
+{
+  constexpr auto heartbeatMessageType = 0x0B00;
+  constexpr auto headerSize = 4;
+  constexpr auto payloadSize = 8;
+  constexpr auto heartbeatMessageSize = headerSize + payloadSize;
+
+  gsize msgLength = 0;
+  auto rawMsg = bytes->get_data(msgLength);
+  auto rawBytes = reinterpret_cast<const uint8_t *>(rawMsg);
+  auto rawWords = reinterpret_cast<const uint16_t *>(rawBytes);
+
+  const auto msgType = rawWords[0];
+
+  if(msgLength == heartbeatMessageSize && msgType == heartbeatMessageType)
+  {
+    auto lpcHeartBeatPtr = reinterpret_cast<const uint64_t *>(rawBytes + payloadSize);
+    auto lpcHeartBeat = *lpcHeartBeatPtr;
+    std::cout << "LPC Heartbeat: " << std::hex << lpcHeartBeat << " type: " << msgType << '\n';
+  }
+}
+
 void LPCReceiver::onDataReceived(Glib::RefPtr<Glib::Bytes> bytes)
 {
   gsize numBytes = 0;
-  auto data = reinterpret_cast<const uint8_t*>(bytes->get_data(numBytes));
+  auto data = reinterpret_cast<const uint8_t *>(bytes->get_data(numBytes));
 
   if(auto numBytesMissing = m_parser->parse(data, numBytes))
   {
@@ -27,16 +49,13 @@ void LPCReceiver::onDataReceived(Glib::RefPtr<Glib::Bytes> bytes)
   {
     g_assert(m_parser->isFinished());
 
-    auto msg = interceptHeartbeat(m_parser->getMessage());
+    auto message = m_parser->getMessage();
 
-    {
-      gsize msgSize = 0;
-      auto msgData = reinterpret_cast<const uint8_t*>(msg->get_data(msgSize));
-      auto heartBeatPtr = reinterpret_cast<const uint64_t*>(msgData + 4);
-      std::cout << std::hex << std::setw(2) << std::setfill('0') << *heartBeatPtr << " \n";
-    }
+    logHeartbeat(message);
+    message = interceptHeartbeat(message);
+    logHeartbeat(message);
 
-    super::onDataReceived(msg);
+    super::onDataReceived(message);
     m_parser = std::make_unique<MessageParser>();
     setBlockSize(MessageParser::getNumInitialBytesNeeded());
   }
@@ -51,12 +70,12 @@ Glib::RefPtr<Glib::Bytes> LPCReceiver::interceptHeartbeat(Glib::RefPtr<Glib::Byt
 
   gsize msgLength = 0;
   auto rawMsg = msg->get_data(msgLength);
-  auto rawBytes = reinterpret_cast<const uint8_t*>(rawMsg);
-  auto rawWords = reinterpret_cast<const uint16_t*>(rawBytes);
+  auto rawBytes = reinterpret_cast<const uint8_t *>(rawMsg);
+  auto rawWords = reinterpret_cast<const uint16_t *>(rawBytes);
 
   if(msgLength == heartbeatMessageSize && rawWords[0] == heartbeatMessageType)
   {
-    auto lpcHeartBeatPtr = reinterpret_cast<const uint64_t*>(rawBytes + heartbeatMessageHeaderSize);
+    auto lpcHeartBeatPtr = reinterpret_cast<const uint64_t *>(rawBytes + heartbeatMessageHeaderSize);
     auto lpcHeartBeat = *lpcHeartBeatPtr;
     auto chainHeartBeat = lpcHeartBeat + m_heartbeat;
 
