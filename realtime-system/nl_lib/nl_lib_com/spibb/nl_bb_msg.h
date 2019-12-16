742de5f1160aef681216c008d6fafe07c7c78322
KlausStrohhaecker <42267360+KlausStrohhaecker@users.noreply.github.com>
2019-12-16 18:00:21 +0100

    wip

--- realtime-system/nl_lib/nl_lib_com/spibb/nl_bb_msg.h
+++ realtime-system/nl_lib/nl_lib_com/spibb/nl_bb_msg.h
@@ -31,7 +31,7 @@
 // TODO implement the following new types in BBBB/Playground also:
 #define BB_MSG_TYPE_HEARTBEAT   0x0B00  // direction: output; arguments(uint16): 4, 4x uint16 (==uint64)
 #define BB_MSG_TYPE_MUTESTATUS  0x0C00  // direction: output; argument (uint16): 1, 1x bit pattern
-#define BB_MSG_TYPE_RIBBON_CAL  0x0D00  // direction: input; arguments(uint16): N, Nx data
+#define BB_MSG_TYPE_RIBBON_CAL  0x0D00  // direction: input; arguments(uint16): 134, 134x data [2x (33x 34x)]
 #define BB_MSG_TYPE_SENSORS_RAW 0x0E00  // direction: output; arguments(uint16): 13, sensor raw data (see nl_tcd_adc_work.c)
 // for inhouse calibration only:
 #define BB_MSG_TYPE_RIBBON_RAW 0xFF01  // direction: output; arguments(uint16): 2, ribbon raw values
