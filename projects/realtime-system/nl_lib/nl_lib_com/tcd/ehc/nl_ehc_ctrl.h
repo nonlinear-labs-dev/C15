462c53ccffb67e43a21777abbd25fdb288438296
KlausStrohhaecker <42267360+KlausStrohhaecker@users.noreply.github.com>
2020-02-19 20:33:19 +0100

    removed dead DeInit()

--- projects/realtime-system/nl_lib/nl_lib_com/tcd/ehc/nl_ehc_ctrl.h
+++ projects/realtime-system/nl_lib/nl_lib_com/tcd/ehc/nl_ehc_ctrl.h
@@ -13,7 +13,6 @@
 #include <stdint.h>
 
 void NL_EHC_InitControllers(void);
-void NL_EHC_DeInitControllers(void);
 void NL_EHC_ProcessControllers(void);
 void NL_EHC_SetLegacyPedalType(const uint16_t channel, const uint16_t type);
 void NL_EHC_SetLegacyPedalParameterSet(uint16_t const channel, uint16_t paramSet);
