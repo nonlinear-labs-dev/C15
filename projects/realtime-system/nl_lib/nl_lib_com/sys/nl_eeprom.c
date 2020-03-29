4ada92851b2473aa6e366789883bc41e91b7affe
KlausStrohhaecker <42267360+KlausStrohhaecker@users.noreply.github.com>
2020-03-30 01:09:36 +0200

    update EEPROM handling
    - add "clear EEPROM" command
    - implement shadow blocks for fail-safe data (shadow written before main, and used when main is corrupt -- like from power-fail during EEPROM progging)

--- projects/realtime-system/nl_lib/nl_lib_com/sys/nl_eeprom.c
+++ projects/realtime-system/nl_lib/nl_lib_com/sys/nl_eeprom.c
@@ -8,6 +8,7 @@
 #include "sys/nl_eeprom.h"
 #include "eeprom_18xx_43xx.h"
 #include "sys/crc.h"
+#include "spibb/nl_bb_msg.h"
 #include <string.h>
 #include <stdio.h>
 
@@ -22,21 +23,25 @@ typedef struct __attribute__((packed))
 } EEPROM_Block;
 
 #define EEPROM_SIZE             (63 * 128)  // 63 blocks with 128 bytes each, do NOT change
+#define SHADOW_OFFSET           (64 * 128)  // offset to shadow pages
 #define EEPROM_MAX_BLOCKSIZE    (2048)      // 2k max size per block, for local buffer sizing,
-#define NUMBER_OF_EEPROM_BLOCKS (16)        // number of blocks we can handle
+#define NUMBER_OF_EEPROM_BLOCKS (8)         // number of blocks we can handle
 
 #define ALIGN32(X) (((X) + 3) & ~3)  // macro to a align a size to next 4byte multiple
 
-static EEPROM_Block blocks[NUMBER_OF_EEPROM_BLOCKS];        // main blocks
-static EEPROM_Block shadowBlocks[NUMBER_OF_EEPROM_BLOCKS];  // shadow/backup blocks
+static EEPROM_Block blocks[NUMBER_OF_EEPROM_BLOCKS];
 static uint32_t     buffer[EEPROM_MAX_BLOCKSIZE / 4];
 static uint16_t     eepromBusy = 0;
 static uint16_t     transfer;
 static uint16_t     remaining;
 static uint32_t *   pDest;
 static uint32_t *   pSrc;
-static uint16_t     forceAlignNext = 0;
-
+static uint16_t     forceAlignNext    = 0;
+static uint16_t     step              = 0;
+static uint16_t     savedIndex        = 0;
+static uint16_t     hadToUseBackup    = 0;
+static uint16_t     fullEraseRequest  = 0;
+static uint16_t     fullEraseFinished = 0;
 /*****************************************************************************
  * Public types/enumerations/variables
  ****************************************************************************/
@@ -46,7 +51,7 @@ static uint16_t     forceAlignNext = 0;
  ****************************************************************************/
 
 /* Register a block of data for EEPROM access */
-/* returns a handle, or 0 in case of failure */
+/* returns a handle, or 0 in case of failure (too large, or no more blocks) */
 uint16_t NL_EEPROM_RegisterBlock(uint16_t const size, EepromBlockAlign_T align)
 {
   if (size > EEPROM_MAX_BLOCKSIZE)
@@ -56,9 +61,7 @@ uint16_t NL_EEPROM_RegisterBlock(uint16_t const size, EepromBlockAlign_T align)
   for (uint16_t i = 0; i < NUMBER_OF_EEPROM_BLOCKS; i++)
   {
     if (returnHandle == 0 && blocks[i].handle == 0)
-    {                            // first free block found, register it
-      blocks[i].handle = i + 1;  // avoid 0 as a handle
-      blocks[i].size   = size;
+    {  // first free block found, register it
       if (align == EEPROM_BLOCK_ALIGN_TO_PAGE)
       {
         nextFreeOffset = (nextFreeOffset + 127) & ~127;  // bump offset to next page boundary
@@ -72,6 +75,8 @@ uint16_t NL_EEPROM_RegisterBlock(uint16_t const size, EepromBlockAlign_T align)
           forceAlignNext = 0;                              // next block's alignment can be arbitrary again
         }
       }
+      blocks[i].handle = i + 1;  // avoid 0 as a handle
+      blocks[i].size   = size;
       blocks[i].offset = nextFreeOffset;
       returnHandle     = blocks[i].handle;
     }
@@ -111,42 +116,49 @@ static inline void memCopy(uint16_t size, uint32_t *pSrc, uint32_t *pDest)
 /* returns 1 if success, or 0 in case of failure */
 uint16_t NL_EEPROM_ReadBlock(uint16_t const handle, void *const data)
 {
-  uint8_t index = handle - 1;
-  if (handle < 1 || handle > NUMBER_OF_EEPROM_BLOCKS || blocks[index].handle != handle
-      || data == NULL || eepromBusy)
+  hadToUseBackup = 0;
+  uint8_t index  = handle - 1;
+  if (handle < 1 || handle > NUMBER_OF_EEPROM_BLOCKS || blocks[index].handle != handle || data == NULL || fullEraseRequest || NL_EEPROM_Busy())
     return 0;
   uint32_t *startAddr = (uint32_t *) (EEPROM_START + blocks[index].offset);
   if (crcFast((uint8_t const *) (startAddr + 1), blocks[index].size) != *startAddr)
-    return 0;  // CRC failed
+  {  // CRC failed on main block, so try shadow block (which was written before main block)
+    startAddr = (uint32_t *) (EEPROM_START + blocks[index].offset + SHADOW_OFFSET);
+    if (crcFast((uint8_t const *) (startAddr + 1), blocks[index].size) != *startAddr)
+      return 0;          // CRC failed on both, no valid data
+    hadToUseBackup = 1;  // mark mismatch
+  }
   memCopy(blocks[index].size, startAddr + 1, (uint32_t *) data);
   return 1;
 }
 
-/* Start write a block of data to EEPROM */
-/* returns 1 if success, or 0 in case of failure or busy */
-uint16_t NL_EEPROM_StartWriteBlock(uint16_t const handle, void *const data)
+/* get any mismatch in last ReadBlock() */
+uint16_t NL_EEPROM_ReadBlockHadToUseBackup(void)
 {
-  uint8_t index = handle - 1;
-  remaining     = ALIGN32(blocks[index].size);
-  if (handle < 1 || handle > NUMBER_OF_EEPROM_BLOCKS || blocks[index].handle != handle)
-    return 0;
-  if (remaining == 0)
-    return 1;  // no data --> done
-  if (data == NULL || eepromBusy)
-    return 0;
-  remaining += 4;  // account for CRC
+  uint16_t ret   = hadToUseBackup;
+  hadToUseBackup = 0;
+  return ret;
+}
+
+/* Start write a block of data to EEPROM */
+static void StartWriteBlock(uint16_t size, uint32_t *src, uint16_t offset, int raw)
+{
+  remaining = ALIGN32(size);
+  if (!raw)
+    remaining += 4;  // account for CRC
   transfer           = remaining;
-  uint8_t pageOffset = blocks[index].offset & 0x7F;  // offset modulo 128
+  uint8_t pageOffset = offset & 0x7F;  // offset modulo 128
   if (pageOffset + transfer > 128)
     transfer = 128 - pageOffset;  // restrict amount to next 128byte boundary
   // copy
-  pSrc           = (uint32_t *) data;
-  pDest          = (uint32_t *) (EEPROM_START + blocks[index].offset);
-  *pDest++       = crcFast((uint8_t const *) data, blocks[index].size);  // poke CRC in first word
-  uint16_t count = (transfer - 4) >> 2;                                  // words remaining data to transfer, without CRC
+  pSrc  = src;
+  pDest = (uint32_t *) (EEPROM_START + offset);
+  if (!raw)
+    *pDest++ = crcFast((uint8_t const *) src, size);  // poke CRC in first word
+  uint16_t count = (transfer - (raw ? 0 : 4)) >> 2;   // words remaining data to transfer, without CRC
   while (count--)
     *pDest++ = *pSrc++;
-  count = (transfer - 4) & 3;  // any bytes remaining, 0..3, can happen only at the end of data
+  count = transfer & 3;  // any bytes remaining, 0..3, can happen only at the end of data
   if (count)
   {  // we must use 32bit aligned write to write the EEPROM data
     uint32_t word  = 0;
@@ -159,15 +171,17 @@ uint16_t NL_EEPROM_StartWriteBlock(uint16_t const handle, void *const data)
     *pDest++ = word;
   }
   remaining -= transfer;                             // update remaining amount of date, if any
+  eepromBusy = 1;                                    // start multi-page write, if required
   Chip_EEPROM_StartEraseAndProgramPage(LPC_EEPROM);  // start burning the first chunk
-  memCopy(remaining, pSrc, buffer);                  // copy rest of data to local buffer, if any
-  pSrc       = buffer;                               // adjust to new read address
-  eepromBusy = 1;                                    // start multi-page write
-  return 1;
+  if (!raw)
+  {
+    memCopy(remaining, pSrc, buffer);  // copy rest of data to local buffer, if any
+    pSrc = buffer;                     // adjust to new read address
+  }
 }
 
 /* multi-page write */
-void NL_EEPROM_Process()
+void Process()
 {
   if (!eepromBusy)
     return;  // nothing to do
@@ -178,7 +192,7 @@ void NL_EEPROM_Process()
     return;
   transfer = remaining;
   if (transfer > 128)
-    transfer = 128;                //restrict amount to next 128byte boundary
+    transfer = 128;                // restrict amount to next 128byte boundary
   uint16_t count = transfer >> 2;  // words remaining data to transfer
   while (count--)                  // 32bit words
     *pDest++ = *pSrc++;
@@ -195,15 +209,133 @@ void NL_EEPROM_Process()
     *pDest++ = word;
   }
   remaining -= transfer;
-  Chip_EEPROM_StartEraseAndProgramPage(LPC_EEPROM);
   eepromBusy = 1;
+  Chip_EEPROM_StartEraseAndProgramPage(LPC_EEPROM);
+}
+
+/* Start write a block of data to EEPROM */
+/* returns 1 if success, or 0 in case of failure or busy */
+uint16_t NL_EEPROM_StartWriteBlock(uint16_t const handle, void *const data)
+{
+  uint16_t index = handle - 1;
+  uint8_t  total = ALIGN32(blocks[index].size);
+  if (handle < 1 || handle > NUMBER_OF_EEPROM_BLOCKS || blocks[index].handle != handle || fullEraseRequest)
+    return 0;
+  if (total == 0)
+    return 1;  // no data --> done
+  if (data == NULL || NL_EEPROM_Busy())
+    return 0;
+  savedIndex = index;  // keep index for main write later
+  // start write to shadow, with CRC and local buffering
+  StartWriteBlock(total, (uint32_t *) data, blocks[savedIndex].offset + SHADOW_OFFSET, 0);
+  step = 1;  // start step chain
+  return 1;
+}
+
+/* process full erase cycle */
+void FullEraseProcess(void)
+{
+  static uint16_t step     = 0;
+  static uint16_t pageAddr = 0;
+  uint32_t *      pEepromMem;
+
+  switch (step)
+  {
+    case 0:
+      pageAddr = 0;
+      step     = 1;
+    case 1:
+      pEepromMem = (uint32_t *) EEPROM_ADDRESS(pageAddr, 0);
+      for (uint32_t i = 0; i < EEPROM_PAGE_SIZE / 4; i++)
+        pEepromMem[i] = 0;
+      Chip_EEPROM_StartEraseAndProgramPage(LPC_EEPROM);
+      step = 2;
+    case 2:
+      if (!Chip_EEPROM_PollIntStatus(LPC_EEPROM))
+        return;  // still burning
+      if (++pageAddr <= 126)
+        step = 1;
+      else
+      {
+        fullEraseRequest  = 0;
+        fullEraseFinished = 1;
+        step              = 0;
+      }
+  }
+}
+
+/* multi-page write */
+void NL_EEPROM_Process(void)
+{
+  if (fullEraseRequest && step == 0)
+  {
+    FullEraseProcess();
+    step = 0;
+  }
+  if (NL_EEPROM_FullEraseFinished())
+  {
+    BB_MSG_WriteMessage2Arg(BB_MSG_TYPE_NOTIFICATION, NOTIFICATION_ID_CLEAR_EEPROM, 1);
+    BB_MSG_SendTheBuffer();
+  }
+  if (step == 0)
+    return;
+  switch (step)
+  {
+    case 1:  // wait for shadow write finished
+      Process();
+      if (eepromBusy)
+        return;  // still writing
+      // now copy shadow block to main block, no CRC and no buffering required
+      StartWriteBlock(ALIGN32(blocks[savedIndex].size),
+                      (uint32_t *) (EEPROM_START + blocks[savedIndex].offset + SHADOW_OFFSET),
+                      blocks[savedIndex].offset, 1);  // start write to main, no buffering
+      step = 2;
+      return;
+    case 2:  // wait for main write finished
+      Process();
+      if (eepromBusy)
+        return;  // still writing
+      step = 0;  // job done, reset chain
+      return;
+  }
 }
 
 /* Check EEPROM is busy (write not finished) */
 /* returns != 0 if busy, 0 if not busy (or failure */
 uint16_t NL_EEPROM_Busy(void)
 {
-  return eepromBusy;
+  return step != 0 || eepromBusy;
+}
+
+/* Erase complete EEPROM */
+static void FullEraseBlocking(void)
+{
+  for (uint32_t pageAddr = 0; pageAddr < 126; pageAddr++)
+  {
+    uint32_t *pEepromMem = (uint32_t *) EEPROM_ADDRESS(pageAddr, 0);
+    for (uint32_t i = 0; i < EEPROM_PAGE_SIZE / 4; i++)
+      pEepromMem[i] = 0;
+    Chip_EEPROM_EraseAndProgramPage(LPC_EEPROM);
+  }
+}
+
+/* start a full EEPROM erase */
+void NL_EEPROM_RequestFullErase(void)
+{
+  if (!fullEraseRequest)
+  {
+    fullEraseRequest  = 1;
+    fullEraseFinished = 0;
+  }
+}
+
+/* return 1 if full erase has finished */
+int NL_EEPROM_FullEraseFinished(void)
+{
+  if (!fullEraseFinished)
+    return 0;
+  fullEraseFinished = 0;
+  return 1;
 }
 
 /* Initialize EEPROM access */
@@ -211,6 +343,9 @@ void NL_EEPROM_Init(void)
 {
   Chip_EEPROM_Init(LPC_EEPROM);
   Chip_EEPROM_SetAutoProg(LPC_EEPROM, EEPROM_AUTOPROG_OFF);
+
+  //  FullEraseBlocking();
+
 #if 0  // test code
   NL_EEPROM_RegisterBlock(113, EEPROM_BLOCK_NO_ALIGN);
   NL_EEPROM_RegisterBlock(12,  EEPROM_BLOCK_ALIGN_TO_PAGE);
--- projects/realtime-system/nl_lib/nl_lib_com/sys/nl_eeprom.h
+++ projects/realtime-system/nl_lib/nl_lib_com/sys/nl_eeprom.h
@@ -26,14 +26,24 @@ void NL_EEPROM_Init(void);  // initialize EEPROM for use
 // align set to EEPROM_BLOCK_ALIGN_TO_PAGE forced this and the next assigned blocks to start at a page
 uint16_t NL_EEPROM_RegisterBlock(uint16_t const size, EepromBlockAlign_T align);
 
-uint16_t NL_EEPROM_ReadBlock(uint16_t const handle, void *const data);  // data start adr must be 4-byte aligned
+// data start adr must be 4-byte aligned
+// returns 0 if error or busy
+uint16_t NL_EEPROM_ReadBlock(uint16_t const handle, void *const data);
 
-uint16_t NL_EEPROM_StartWriteBlock(uint16_t const handle, void *const data);  // data start adr must be 4-byte aligned
+// returns 1 if there was a main vs. shadow mismatch at last read
+// flag is cleared by this call, and force-set by ReadBlock()
+uint16_t NL_EEPROM_ReadBlockHadToUseBackup(void);
 
-// check this before any new read or write
+// data start adr must be 4-byte aligned
+// returns 0 if error or busy
+uint16_t NL_EEPROM_StartWriteBlock(uint16_t const handle, void *const data);
+
+// check this before any new read/write as it might be ignored if still busy
 // if the block was registered with page-alignment, though, read is always save and cannot corrupt a pending write
 uint16_t NL_EEPROM_Busy(void);
 
-void NL_EEPROM_Process();  // cyclic task to perform multi-page writes
+void NL_EEPROM_Process(void);            // cyclic task to perform the multi-page writes, or a full EEPROM erase
+void NL_EEPROM_RequestFullErase(void);   // initiate a full EEPROM erase
+int  NL_EEPROM_FullEraseFinished(void);  // flag finished full EEPROM erase
 
 #endif
