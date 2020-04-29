02e66a4c372a0e28bf0bb0457409840d493393a4
KlausStrohhaecker <42267360+KlausStrohhaecker@users.noreply.github.com>
2020-04-29 19:11:26 +0200

    Update nl_cgu.h
    add some commonly used timing-related constants

--- projects/realtime-system/nl_lib/nl_lib_com/drv/nl_cgu.h
+++ projects/realtime-system/nl_lib/nl_lib_com/drv/nl_cgu.h
@@ -7,7 +7,13 @@
 #ifndef NL_CGU_H
 #define NL_CGU_H
 
-#define NL_LPC_CLK       204000000
-#define NL_LPC_AUDIO_CLK 12288000
+#define NL_LPC_CLK                  (204000000ul)
+#define NL_LPC_CLK_PERIOD_100PS     (10000000000ul / NL_LPC_CLK)
+#define NL_LPC_AUDIO_CLK            (12288000ul)
+#define M0_PERIOD_NS                (125000ul)
+#define M0_PERIOD_US                (M0_PERIOD_NS / 1000ul)
+#define M0_SYSTICKS_PER_PERIOD      (128ul)  // 2^n !! requested systicks per 125us period
+#define M0_SYSTICKS_PER_PERIOD_MASK (M0_SYSTICKS_PER_PERIOD - 1ul)
+#define M0_SYSTICK_IN_NS            (M0_PERIOD_NS / M0_SYSTICKS_PER_PERIOD)  // period of one systick
 
 #endif
