f8bd4001204d09f12ddc7dd1b536abbae6998046
KlausStrohhaecker <42267360+KlausStrohhaecker@users.noreply.github.com>
2020-05-07 20:15:36 +0200

    update nl_spi_dma.*
    - avoid runtime-emulated division instructions (M0 core)
    - needed to inline for the same reason

--- projects/realtime-system/nl_lib/nl_lib_com/drv/nl_spi_dma.h
+++ projects/realtime-system/nl_lib/nl_lib_com/drv/nl_spi_dma.h
@@ -14,10 +14,50 @@
 #include "cmsis/lpc43xx_ssp.h"
 #include "cmsis/lpc_types.h"
 #include "drv/nl_gpdma.h"
+#include "drv/nl_cgu.h"
 
 #define SPI_MODE_MASTER ((uint32_t)(0))
 
-void SPI_DMA_Init(LPC_SSPn_Type *SSPx, uint32_t mode, uint32_t clk_rate);
+/**********************************************************************
+ * @brief		Initializes the SPI-DMA driver for the desired SSP
+ * @param[in]	SSPx	Pointer to selected SSP peripheral, should be:
+ * 					- LPC_SSP0	:SSP0 peripheral
+ * 					- LPC_SSP1	:SSP1 peripheral
+ * @param[in]	mode	SPI Mode, should be:
+ * 					- SSP_MASTER_MODE	:SPI master
+ * 					- SSP_SLAVE_MODE	:SPI slave
+ * @param[in]	clk_rate	Clock Rate in Hz
+ **********************************************************************/
+inline void SPI_DMA_Init(LPC_SSPn_Type *const SSPx, uint32_t const mode, uint32_t const clk_rate)
+{
+  uint32_t prescale, cr0_div;
+
+  if ((SSPx != LPC_SSP0) && (SSPx != LPC_SSP1))
+    return;
+
+  SSPx->CR0 = SSP_CPHA_SECOND | SSP_CPOL_HI | SSP_FRAME_SPI | SSP_DATABIT_8;
+  SSPx->CR1 = mode;
+
+  // Set clock rate for SSP peripheral
+  // Find pre-scaler and divider values without using divisions at runtime (M0 core has no DIV instruction)
+  for (prescale = 2; prescale < 256; prescale += 2)
+    if (NL_LPC_CLK / clk_rate < 256 * prescale)
+    {  // found a pre-scaler that results in a divider less < 256
+      for (cr0_div = 0; cr0_div < 256; cr0_div++)
+        if (prescale * (cr0_div + 1) >= NL_LPC_CLK / clk_rate)
+          goto found;  // found a divider that results in the first clock-rate >= required
+    }
+  // code should never come to here, make sure you call with realistic target clock rates to avoid fails
+
+found:
+  /* Write computed prescaler and divider back to register */
+  SSPx->CR0 &= (~SSP_CR0_SCR(0xFF)) & SSP_CR0_BITMASK;
+  SSPx->CR0 |= (SSP_CR0_SCR(cr0_div)) & SSP_CR0_BITMASK;
+  SSPx->CPSR = prescale & SSP_CPSR_BITMASK;
+
+  SSPx->DMACR |= (SSP_DMA_TX | SSP_DMA_RX);
+  SSPx->CR1 |= SSP_CR1_SSP_EN;
+}
 
 void SPI_DMA_SwitchMode(LPC_SSPn_Type *SSPx, uint32_t pol_pha);
 void SPI_DMA_SwitchModeNonBlocking(LPC_SSPn_Type *SSPx, uint32_t pol_pha);
