fcd12bc204dd5bc4099157bf16cc6cbcc251da43
KlausStrohhaecker <42267360+KlausStrohhaecker@users.noreply.github.com>
2020-05-07 20:18:33 +0200

    update nl_espi_core.*
    - avoid runtime-emulated division instructions (M0 core)
    - needed to inline for the same reason

--- projects/realtime-system/nl_lib/nl_lib_com/espi/nl_espi_core.h
+++ projects/realtime-system/nl_lib/nl_lib_com/espi/nl_espi_core.h
@@ -27,7 +27,14 @@ typedef struct
   GPIO_NAME_T* sap;
 } ESPI_PINS_T;
 
-void ESPI_Init(uint32_t clkRateInHz);
+extern LPC_SSPn_Type* ESPI_SSP;
+
+inline void ESPI_Init(uint32_t clkRateInHz)
+{
+  /** init SSP peripheral */
+  SPI_DMA_Init(ESPI_SSP, SPI_MODE_MASTER, clkRateInHz);
+}
+
 void ESPI_Config(LPC_SSPn_Type* SSPx, ESPI_PINS_T* espi_pins);
 
 void     ESPI_SAP_Set(void);
