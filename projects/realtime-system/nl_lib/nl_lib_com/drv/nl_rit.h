9fc2cde71d91e3edd1495e2e1f67d8fb5011dc86
KlausStrohhaecker <42267360+KlausStrohhaecker@users.noreply.github.com>
2020-05-07 20:12:46 +0200

    RIT_Init_* inlined to avoid runtime-emulated division instructions (M0 core)

--- projects/realtime-system/nl_lib/nl_lib_com/drv/nl_rit.h
+++ projects/realtime-system/nl_lib/nl_lib_com/drv/nl_rit.h
@@ -11,9 +11,53 @@
 
 #include <stdint.h>
 #include "cmsis/LPC43xx.h"
+#include "drv/nl_cgu.h"
 
-void RIT_Init_IntervalInUs(uint32_t time_us);
-void RIT_Init_IntervalInNs(uint32_t ns);
+inline void RIT_Init_IntervalInUs(uint32_t const time_us)
+{
+  uint32_t cmp_value;
+
+  /* Initialize RITimer */
+  LPC_RITIMER->COMPVAL = 0xFFFFFFFF;
+  LPC_RITIMER->MASK    = 0x00000000;
+  LPC_RITIMER->CTRL    = 0x0C;
+  LPC_RITIMER->COUNTER = 0x00000000;
+
+  /* Determine approximate compare value based on clock rate and passed interval */
+  cmp_value = (uint32_t)(NL_LPC_CLK / 1000000ul * time_us);
+
+  /* Set timer compare value */
+  LPC_RITIMER->COMPVAL = cmp_value;
+
+  /* Set timer enable clear bit to clear timer to 0 whenever
+	 * counter value equals the contents of RICOMPVAL */
+  LPC_RITIMER->CTRL |= (1 << 1);
+
+  NVIC_EnableIRQ(RITIMER_IRQn);
+}
+
+inline void RIT_Init_IntervalInNs(uint32_t const ns)
+{
+  uint32_t cmp_value;
+
+  /* Initialize RITimer */
+  LPC_RITIMER->COMPVAL = 0xFFFFFFFF;
+  LPC_RITIMER->MASK    = 0x00000000;
+  LPC_RITIMER->CTRL    = 0x0C;
+  LPC_RITIMER->COUNTER = 0x00000000;
+
+  /* Determine approximate compare value based on clock rate and passed interval */
+  cmp_value = (uint32_t)(((NL_LPC_CLK / 1000000ul) * ns) / 1000ul);
+
+  /* Set timer compare value */
+  LPC_RITIMER->COMPVAL = cmp_value;
+
+  /* Set timer enable clear bit to clear timer to 0 whenever
+	 * counter value equals the contents of RICOMPVAL */
+  LPC_RITIMER->CTRL |= (1 << 1);
+
+  NVIC_EnableIRQ(RITIMER_IRQn);
+}
 
 static inline void RIT_ClearInt(void)
 {
