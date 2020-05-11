74bbde9ce1cac16fc02aa59881dd26f8d5d29f46
KlausStrohhaecker <42267360+KlausStrohhaecker@users.noreply.github.com>
2020-05-11 17:55:32 +0200

    Update nl_cgu.h
    - reorganize system timing setup constants

--- projects/realtime-system/nl_lib/nl_lib_com/drv/nl_cgu.h
+++ projects/realtime-system/nl_lib/nl_lib_com/drv/nl_cgu.h
@@ -7,13 +7,14 @@
 #ifndef NL_CGU_H
 #define NL_CGU_H
 
-#define NL_LPC_CLK                  (204000000ul)
-#define NL_LPC_CLK_PERIOD_100PS     (10000000000ul / NL_LPC_CLK)
-#define NL_LPC_AUDIO_CLK            (12288000ul)
-#define M0_PERIOD_NS                (125000ul)
-#define M0_PERIOD_US                (M0_PERIOD_NS / 1000ul)
-#define M0_SYSTICKS_PER_PERIOD      (128ul)  // 2^n !! requested systicks per 125us period
-#define M0_SYSTICKS_PER_PERIOD_MASK (M0_SYSTICKS_PER_PERIOD - 1ul)
-#define M0_SYSTICK_IN_NS            (M0_PERIOD_NS / M0_SYSTICKS_PER_PERIOD)  // period of one systick
+#define NL_LPC_CLK              (204000000ul)
+#define NL_LPC_CLK_PERIOD_100PS (10000000000ul / NL_LPC_CLK)
+#define NL_LPC_AUDIO_CLK        (12288000ul)
+
+#define M4_PERIOD_US (5ul)  // M0 interrupt period in 1us multiples
+#define M4_FREQ_HZ   (1000000 / M4_PERIOD_US)
+
+#define M0_PERIOD_62_5NS (16ul)  // M0 interrupt period in 62.5ns multiples
+#define M0_FREQ_HZ       (16000000ul / M0_PERIOD_62_5NS)
 
 #endif
