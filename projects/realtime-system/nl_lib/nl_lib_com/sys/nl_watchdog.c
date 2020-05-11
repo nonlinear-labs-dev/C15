dcfa41e9ffb1d675f88dc97d5354a96c5b62126d
KlausStrohhaecker <42267360+KlausStrohhaecker@users.noreply.github.com>
2020-05-11 17:44:58 +0200

    rename watchdog function files

--- projects/realtime-system/nl_lib/nl_lib_com/sys/nl_watchdog.c
+++ projects/realtime-system/nl_lib/nl_lib_com/sys/nl_watchdog.c
@@ -1,17 +1,19 @@
 /******************************************************************************/
-/** @file		nl_ticker.c
-    @date		2020-01-27
+/** @file		nl_watchdog.c
+    @date		2020-05-10
     @version	0
     @author		KSTR
-    @brief		system ticker global variable
+    @brief		watchdog
 *******************************************************************************/
-#include "sys/nl_ticker.h"
+#include "sys/nl_watchdog.h"
 #include "cmsis/LPC43xx.h"
 
-volatile uint32_t SYS_ticker = 0;
+static int watchDogEnabled = 0;
 
 void SYS_WatchDogClear(void)
 {
+  if (!watchDogEnabled)
+    return;
   __disable_irq();
   LPC_WWDT->FEED = 0xAA;  // the required feed value sequence ...
   LPC_WWDT->FEED = 0x55;  // ... to reload the watchdog counter
@@ -21,13 +23,18 @@ void SYS_WatchDogClear(void)
 void SYS_WatchDogInit(uint32_t timeoutInMs)
 {
   // Timeout = T_WDCLK*4 * TC = 1/3Mhz * TC --> TC = Timeout(s) * 3MHz = Timeout(ms) * 3kHz
-  LPC_WWDT->TC  = 3000ul * timeoutInMs;
-  LPC_WWDT->MOD = WWDT_MOD_WDEN_Msk | WWDT_MOD_WDRESET_Msk;
+  __disable_irq();
+  LPC_WWDT->TC    = 3000ul * timeoutInMs;
+  LPC_WWDT->MOD   = WWDT_MOD_WDEN_Msk | WWDT_MOD_WDRESET_Msk;
+  watchDogEnabled = 1;
+  __enable_irq();
   SYS_WatchDogClear();
 }
 
 void SYS_Reset(void)
 {
+  if (!watchDogEnabled)
+    return;
   __disable_irq();
   LPC_WWDT->FEED = 0x00;  // invalid feed sequence ...
   LPC_WWDT->FEED = 0x00;  // ... causes instant reset
