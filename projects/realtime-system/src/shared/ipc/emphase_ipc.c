11986a1ae0497f7393b03b44957f2763ef32117e
KlausStrohhaecker <42267360+KlausStrohhaecker@users.noreply.github.com>
2020-05-24 21:59:26 +0200

    Update emphase_ipc.c
    - remove uneccesary #include
    - updated dependancy comment

--- projects/realtime-system/src/shared/ipc/emphase_ipc.c
+++ projects/realtime-system/src/shared/ipc/emphase_ipc.c
@@ -1,17 +1,17 @@
 /******************************************************************************/
-/** @file		emphase_ipc.h
-    @date		2016-02-29 DTZ
+/** @file		emphase_ipc.c
+    @date		2020-05-24 KSTR
     @brief    	M4 <-> M0 inter processor communication module
     @author		Nemanja Nikodijevic 2015-02-03
 *******************************************************************************/
 #include <stdint.h>
 #include "ipc/emphase_ipc.h"
-#include "drv/nl_rit.h"
 
 //
 // Places the variable(s) in the shared memory block.
 // This block must be defined in C15_LPC4337_MEM.ld.
-// The section ".shared" must be defined in both C15_LPC4337_M0.ld and C15_LPC4337_M4.ld identically.
+// The section used must be defined in both C15_LPC4337_M0.ld and C15_LPC4337_M4.ld identically
+// and must be defined properly and identically in the *_MEM.ld script includes.
 // When the size of all shared variables is larger than the block the linker will throw an error.
 // To make sure all variable are at the same memory location and have the same layout,
 // use only one single struct and embed all your variables there.
