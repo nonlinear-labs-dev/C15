f6b0df773a95ed127efeed30092b2f986014047f
KlausStrohhaecker <klaus.strohhaecker@nonlinear-labs.de>
2020-08-17 16:18:44 +0200

    wip

--- projects/audio-engine/src/io/midi/AlsaMidiInput.cpp
+++ projects/audio-engine/src/io/midi/AlsaMidiInput.cpp
@@ -47,8 +47,8 @@ void AlsaMidiInput::doBackgroundWork()
 {
   uint8_t byte;
 
-  // snd_midi_event_t *parser = nullptr;
-  // snd_midi_event_new(512, &parser);
+  snd_midi_event_t *parser = nullptr;
+  snd_midi_event_new(512, &parser);  // a chunk of raw SysEx will at most be this size
 
   int numPollFDs = snd_rawmidi_poll_descriptors_count(m_handle);
 
@@ -76,83 +76,42 @@ void AlsaMidiInput::doBackgroundWork()
       break;
 
     MidiEvent e;
+    snd_seq_event_t event;
     int rawIdx = 0;
 
     while(m_run)
     {
       auto readResult = snd_rawmidi_read(m_handle, &byte, 1);
 
-      if(readResult == 1)
-      {  // got one byte
-        // printf("\n%02X\n", byte);
-        switch(rawIdx)
-        {
-          case 0:  // waiting for MIDI Msg "Pitchbend"
-            if((byte & 0xF0) == 0xE0)
-              e.raw[rawIdx++] = byte;
-            break;
-          case 1:
-          case 2:  // remaining 2 bytes
-            if(byte < 0x80)
-            {
-              e.raw[rawIdx++] = byte;
-              if(rawIdx == 3)
-              {  // last byte
-                rawIdx = 0;
-                getCallback()(e);
-                goto Done;
-              }
-            }
-            break;
-        }
-      }
-      else if(readResult == -19)
-      {
+      if(readResult == -19)
         nltools::throwException("Could not read from midi input file descriptor =>", snd_strerror(readResult));
-      }
-    }
-  Done:;
-  }
-}
+      if(readResult != 1)  // one byte successfully read ?
+        continue;          // ... no
 
-/*
-        if(rawIdx < 3)
-          e.raw[rawIdx++] = byte;
+      if(rawIdx < 3)             // we record raw bytes here because pitchbend event data "event.data.control.*" ...
+        e.raw[rawIdx++] = byte;  // ... does not nicely cover the 14 bit value range
 
-        snd_seq_event_t event;
-
-        if(snd_midi_event_encode_byte(parser, byte, &event) == 1)
+      if(snd_midi_event_encode_byte(parser, byte, &event) == 1)
+      {  // have a finished MIDI message
+        switch(event.type)
         {
-          if(event.type != SND_SEQ_EVENT_NONE)
-          {
-            if(event.type == SND_SEQ_EVENT_SYSEX)
-            {
-#warning TODO : decode and proxy SysEx data to PG, containing BB messages
-#if 0
-              uint8_t *p = (uint8_t *) event.data.ext.ptr;
-              printf("\nSysEx Data (%i bytes) : ", event.data.ext.len);
-              for(int i = 0; i < event.data.ext.len; i++)
-              {
-                printf("%02X ", p[i]);
-              }
-              printf("\n");
-              fflush(stdout);
-#endif
-            }
-            else  // all other MIDI events go to AE's handler
-            {
-#if 0
-              if(event.type == SND_SEQ_EVENT_PITCHBEND)
-              {  // our own data ?
-                printf("\nTCD Data : %02X %02X %02X\n", e.raw[0], e.raw[1], e.raw[2]);
-                fflush(stdout);
-              }
-#endif
-              getCallback()(e);
-              break;
-            }
-          }
-
-          rawIdx = 0;
+          case SND_SEQ_EVENT_PITCHBEND:  // TCD data (wrapped in PitchBends)
+            printf("\nTCD Data : %02X %02X %02X\n", e.raw[0], e.raw[1], e.raw[2]);
+            fflush(stdout);
+            getCallback()(e);
+            break;
+          case SND_SEQ_EVENT_SYSEX:  // chunck of SysEx data (may not be a complete packet, scan for terminating "0xF7")
+            uint8_t *p = (uint8_t *) event.data.ext.ptr;
+            printf("\nSysEx Data (%i bytes) : ", event.data.ext.len);
+            for(int i = 0; i < event.data.ext.len; i++)
+              printf("%02X ", p[i]);
+            printf("\n");
+            fflush(stdout);
+            break;
         }
- */
+        rawIdx = 0;
+        break;  // break out of "while(m_run){}" after ANY event, seems to be required to keep thread from stalling ???              }
+      }
+    }  // while(m_run)
+  }    // while(true)
+}
