7b27773de5a089155d99a2fce1d89fea6022c93e
Klaus Strohhaecker <klaus.strohhaecker@nonlinear-labs.de>
2020-10-12 15:16:01 +0200

    remove unneeded files

--- projects/midibridge/firmware/src/co-proc/src/espi/dev/nl_espi_dev_aftertouch.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void     ESPI_DEV_Aftertouch_Init(void);
-void     ESPI_DEV_Aftertouch_EspiPull(void);
-uint32_t ESPI_DEV_Aftertouch_EspiPullRet(void);
-uint16_t ESPI_DEV_Aftertouch_GetValue(void);
