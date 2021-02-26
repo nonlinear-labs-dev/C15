640029af34a8d5c4baabb5aa6667829e7ed2e336
Klaus Strohhaecker <klaus.strohhaecker@nonlinear-labs.de>
2021-02-26 16:50:03 +0100

    board-version.c : check for applicable hardware revision
    Sadly, only on V7.2++ uniboards the board version can be read our correctly.
    Root cause is a hidden and unknown side-effect of our device tree that keeps some GPIOs on the BBB from working properly, even depending on BBB revision.

--- projects/supervisor/supplements/board-version/board-version.c
+++ projects/supervisor/supplements/board-version/board-version.c
@@ -96,42 +96,105 @@ static int linuxgpio_openfd(unsigned int gpio)
 }
 
 // -----------------------------------------------------------------------------------------------------
-#define BIT0         (46u)    // port number bit 0
-#define BIT1         (27u)    // port number bit 1
-#define BIT2         (65u)    // port number bit 2
-#define BIT3         (22u)    // port number bit 3
-#define WAIT_TIME_US (10000u)  // wait time for IO-accesses to settle
+#define BIT0 (46u)  // port number bit 0
+#define BIT1 (27u)  // port number bit 1
+//#define BIT2         (65u)    // port number bit 2   // (old hardware, not usable with BBB with our device tree)
+#define BIT2       (76u)  // port number bit 2   // new V7.2 hardware
+#define BIT3       (22u)  // port number bit 3
+#define V7_2_TEST1 (80u)  // one side of V7.2 ID-resistor
+#define V7_2_TEST2 (77u)  // other side of V7.2 ID-resistor
 
+#define WAIT_TIME_US (10000u)  // wait time for IO-accesses to settle
 
 int main(void)
 {
-  int fd[4];
+  int fd[6];
 
   linuxgpio_unexport(BIT0);
   linuxgpio_unexport(BIT1);
   linuxgpio_unexport(BIT2);
   linuxgpio_unexport(BIT3);
+  linuxgpio_unexport(V7_2_TEST1);
+  linuxgpio_unexport(V7_2_TEST2);
 
   linuxgpio_export(BIT0);
   linuxgpio_export(BIT1);
   linuxgpio_export(BIT2);
   linuxgpio_export(BIT3);
+  linuxgpio_export(V7_2_TEST1);
+  linuxgpio_export(V7_2_TEST2);
 
   linuxgpio_dir_in(BIT0);
   linuxgpio_dir_in(BIT1);
   linuxgpio_dir_in(BIT2);
   linuxgpio_dir_in(BIT3);
+  linuxgpio_dir_out(V7_2_TEST1);
+  linuxgpio_dir_in(V7_2_TEST2);
 
   usleep(WAIT_TIME_US);
   fd[0] = linuxgpio_openfd(BIT0);
   fd[1] = linuxgpio_openfd(BIT1);
   fd[2] = linuxgpio_openfd(BIT2);
   fd[3] = linuxgpio_openfd(BIT3);
+  fd[4] = linuxgpio_openfd(V7_2_TEST1);
+  fd[5] = linuxgpio_openfd(V7_2_TEST2);
   usleep(WAIT_TIME_US);
-  
+
   int  boardId = 0;
   char c;
 
+#if 01
+  // check if hardware is >= V7.2
+  if (write(fd[4], "1", 1) != 1)
+  {
+    perror("Write to GPIO failed");
+    goto Error;
+  }
+  usleep(WAIT_TIME_US);
+  if (read(fd[5], &c, 1) != 1)
+  {
+    perror("Read from GPIO failed");
+    goto Error;
+  }
+  if (c != '1' && c != '0')
+  {
+    perror("Read from GPIO was neither 0 nor 1");
+    printf("c = %02Xh\n", c);
+    goto Error;
+  }
+  if (c == 0)
+  {
+    boardId = -1;  // < V7.2, board Id cannot be detected
+    goto Done;
+  }
+  close(fd[5]);
+  fd[5] = linuxgpio_openfd(V7_2_TEST2);
+
+  if (write(fd[4], "0", 1) != 1)
+  {
+    perror("Write to GPIO failed");
+    goto Error;
+  }
+  usleep(WAIT_TIME_US);
+  if (read(fd[5], &c, 1) != 1)
+  {
+    perror("Read from GPIO failed");
+    goto Error;
+  }
+  if (c != '1' && c != '0')
+  {
+    perror("Read from GPIO was neither 0 nor 1");
+    printf("c = %02Xh\n", c);
+    goto Error;
+  }
+  if (c == '1')
+  {
+    boardId = -1;  // < V7.2, board Id cannot be detected
+    goto Done;
+  }
+#endif
+
+  // read in board version bits
   if (read(fd[0], &c, 1) != 1)
     goto Error;
   if (c == '1')
@@ -169,10 +232,14 @@ Done:
   close(fd[1]);
   close(fd[2]);
   close(fd[3]);
+  close(fd[4]);
+  close(fd[5]);
   linuxgpio_unexport(BIT0);
   linuxgpio_unexport(BIT1);
   linuxgpio_unexport(BIT2);
   linuxgpio_unexport(BIT3);
+  linuxgpio_unexport(V7_2_TEST1);
+  linuxgpio_unexport(V7_2_TEST2);
 
   return boardId;
 }
