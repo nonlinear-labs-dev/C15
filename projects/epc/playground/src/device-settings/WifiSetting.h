f171b0881ec1c5cca36858396c79c18499cf2d98
schlenkibus <justus.scheil@parteimail.de>
2021-07-08 12:40:25 +0200

    removed polling code

--- projects/epc/playground/src/device-settings/WifiSetting.h
+++ projects/epc/playground/src/device-settings/WifiSetting.h
@@ -3,45 +3,20 @@
 #include "EpcWifi.h"
 #include "NLEnumSetting.h"
 
-ENUM(WifiSettings, int, Disabled, Enabled, Querying)
-
-class WifiSetting;
-
-class WiFiPollImpl {
- public:
-  explicit WiFiPollImpl(WifiSetting* s);
-  virtual bool poll() = 0;
- protected:
-  WifiSetting* m_setting;
-};
-
-class EPC2WiFiPollImpl : public WiFiPollImpl {
- public:
-  using WiFiPollImpl::WiFiPollImpl;
-  bool poll() override;
-};
-
-class BBBWiFiPollImpl : public WiFiPollImpl {
- public:
-  using WiFiPollImpl::WiFiPollImpl;
-  bool poll() override;
-};
+ENUM(WifiSettings, int, Disabled, Enabled)
 
 class WifiSetting : public NLEnumSetting<WifiSettings>, public sigc::trackable
 {
  public:
   typedef NLEnumSetting<WifiSettings> super;
-  explicit WifiSetting(UpdateDocumentContributor& settings, const std::shared_ptr<EpcWifi>& localWifi);
+  explicit WifiSetting(UpdateDocumentContributor& settings, std::shared_ptr<EpcWifi>  localWifi);
 
   bool set(tEnum m) override;
   bool persistent() const override;
 
  private:
   void enableDisableBBBWifi(tEnum state);
-  bool pollAccessPointRunning();
 
   sigc::connection m_pollConnection;
   const std::shared_ptr<EpcWifi> m_localWifi;
-
-  std::unique_ptr<WiFiPollImpl> m_pollImpl;
 };
