c87a4b81234ba7870f13d933c7d6471624bea6fb
schlenkibus <justus.scheil@parteimail.de>
2021-07-08 12:40:25 +0200

    removed polling code

--- projects/epc/playground/src/device-settings/WifiSetting.cpp
+++ projects/epc/playground/src/device-settings/WifiSetting.cpp
@@ -2,8 +2,8 @@
 #include <nltools/system/SpawnCommandLine.h>
 #include <nltools/Types.h>
 #include <nltools/logging/Log.h>
-#include <glibmm.h>
 #include <playground.h>
+#include <utility>
 
 std::vector<std::string> getArgs(WifiSettings s)
 {
@@ -37,17 +37,10 @@ std::vector<std::string> getArgs(WifiSettings s)
              "accesspoint;" };
 };
 
-WifiSetting::WifiSetting(UpdateDocumentContributor& settings, const std::shared_ptr<EpcWifi>& localWifi)
-    : super(settings, WifiSettings::Querying)
-    , m_localWifi(localWifi)
+WifiSetting::WifiSetting(UpdateDocumentContributor& settings, std::shared_ptr<EpcWifi> localWifi)
+    : super(settings, WifiSettings::Enabled)
+    , m_localWifi(std::move(localWifi))
 {
-  static auto isEpc2 = !strcmp(TARGET_PLATFORM, "epc2");
-  if(isEpc2)
-    m_pollImpl = std::make_unique<EPC2WiFiPollImpl>(this);
-  else
-    m_pollImpl = std::make_unique<BBBWiFiPollImpl>(this);
-
-  pollAccessPointRunning();
 }
 
 bool WifiSetting::set(tEnum m)
@@ -61,74 +54,21 @@ bool WifiSetting::set(tEnum m)
     m_localWifi->setNewWifiState(false);
 
   enableDisableBBBWifi(m);
-  pollAccessPointRunning();
 
   return ret;
 }
 
 bool WifiSetting::persistent() const
 {
-  return false;
-}
-
-bool WifiSetting::pollAccessPointRunning()
-{
-  if constexpr(!isDevelopmentPC)
-  {
-    if(m_pollImpl)
-    {
-      return m_pollImpl->poll();
-    }
-  }
-
-  return false;
+  return true;
 }
 
 void WifiSetting::enableDisableBBBWifi(WifiSettings m)
 {
-  if(m != WifiSettings::Querying)
-  {
+  if constexpr(!isDevelopmentPC) {
     auto bbbWifiEnableDisableArgs = getArgs(m);
-    SpawnAsyncCommandLine::spawn(
-        bbbWifiEnableDisableArgs, [](auto str) { nltools::Log::error(__LINE__, "succ:", str); },
-        [](auto err) { nltools::Log::error(__LINE__, "err:", err); });
+    SpawnAsyncCommandLine::spawn(bbbWifiEnableDisableArgs,
+      [](auto str) { nltools::Log::error(__LINE__, "succ:", str); },
+      [](auto err) { nltools::Log::error(__LINE__, "err:", err); });
   }
-}
-
-WiFiPollImpl::WiFiPollImpl(WifiSetting* setting)
-    : m_setting { setting }
-{
-}
-
-bool EPC2WiFiPollImpl::poll()
-{
-  SpawnAsyncCommandLine::spawn(
-      { "nmcli", "-g", "GENERAL.STATE", "con", "show", "C15" },
-      [this](const std::string& ret)
-      {
-        nltools::Log::error("nmcli says:", ret);
-        m_setting->set(ret == "activated" ? WifiSettings::Enabled : WifiSettings::Disabled);
-      },
-      [](const std::string& ret) { nltools::Log::error(ret); });
-  return false;
-}
-
-bool BBBWiFiPollImpl::poll()
-{
-  std::vector<std::string> argsBBB { "/usr/bin/ssh", "-o",        "StrictHostKeyChecking=no", "root@192.168.10.11",
-                                     "systemctl",    "is-active", "accesspoint.service" };
-  SpawnAsyncCommandLine::spawn(
-      argsBBB,
-      [this](const std::string& ret)
-      {
-        nltools::Log::error(__LINE__, ret);
-        m_setting->set(WifiSettings::Enabled);
-      },
-      [this](const std::string& err)
-      {
-        nltools::Log::error(__LINE__, "err:", err);
-        m_setting->set(WifiSettings::Enabled);
-      });
-
-  return false;
 }
